cd4cbcba20acc920f6206a49deed0649d7ae7df0
shoaib-karim <shoaibkarim159@gmail.com>
2024-01-18 03:39:45 +0600

    Popup text changed, link added to openup a new window, css adjustment for the link.

--- src/app.jsx
+++ src/app.jsx
@@ -12,16 +12,30 @@ const App = () => {
     // console.log("messageFromContent:", messageFromContent);
   }, [messageFromContent]);
 
+  // Function to handle link click and open in a new tab
+  const handleLinkClick = () => {
+    chrome.tabs.create({
+      url: "https://www.clinical-partners.co.uk/for-adults/adult-adhd-add/test-for-adhd",
+    });
+  };
+
   return (
     <div className="App" data-testid="app">
       <section className="content">
         <img className="graphic" src={graphicSrc} alt="Graphic" />
         <h1>ADHD Bionic Mode</h1>
         <p>{tab?.id}</p>
-        <p>Bionic mode for reader with ADHD. This extension helps users to keep focus.</p>
+        <p>
+          Bionic mode for reader with ADHD. This extension helps users to keep focus while reading
+          blogs in the internet.
+        </p>
         <ActionToggle />
         <span>
-          Scripts Version: {process.env.VERSION} / Build: {process.env.BUILD}
+          Do you have ADHD? Diagnose by yourself here:
+          <br />{" "}
+          <a href="#" onClick={handleLinkClick} id="spanLink">
+            Clinical Partners
+          </a>
         </span>
       </section>
     </div>
